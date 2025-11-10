77bec271c3831a3e2a134c76a288f0b356449288
igorstec <igor@tmsaudit.pl>
2025-11-10 16:32:45 +0100

    Implement a o(nmk) solve compared to o(mn^2k)

--- lepszegrzyby.cpp
+++ lepszegrzyby.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <chrono>
 #include <thread>
+#include <stdbool.h>
 
 using namespace std;
 using namespace chrono;
@@ -22,20 +23,6 @@ long long szybkiePotegowanieModulo(long long podstawa, long long wykladnik, long
     return wynik;
 }
 
-void przesunWMiejscu(vector<int> &v, int przesuniecie) {
-    if (przesuniecie == 0 || v.empty()) return;
-    int pierwszyElement = v[0];
-    int rozmiar = v.size();
-
-    for (int i = rozmiar - 1; i >= przesuniecie; --i) {
-        v[i] = v[i - przesuniecie];
-    }
-
-    for (int i = 0; i < przesuniecie && i < rozmiar; ++i) {
-        v[i] = pierwszyElement;
-    }
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -43,93 +30,102 @@ int main() {
     int n, m, k, g;
     cin >> n >> m >> k >> g;
 
-    vector<vector<int> > grzyby(n, vector<int>(m, 0));
+    vector grzyby(n, vector<bool>(m, false));
 
     for (int i = 0; i < g; i++) {
         int a, b;
         cin >> a >> b;
         a--;
         b--;
-        grzyby[a][b]++;
+        grzyby[a][b] = true;;
     }
 
-    int iloscgrzybowpod = 0;
-    for (int i = 0; i < m; i++) {
-        for (int j = n - 1; j >= 0; j--) {
-            iloscgrzybowpod += grzyby[j][i];
-            grzyby[j][i] = iloscgrzybowpod;
-        }
-        iloscgrzybowpod = 0;
+    if (k == 0) {
+        cout << szybkiePotegowanieModulo(n, m - 1, MOD) << '\n';
+        return 0;
     }
 
     vector<vector<int> > nastepnaKolumna(n, vector<int>(k + 1, 0));
+    vector<vector<int> > nastepnaTemp(n, vector<int>(k + 1, 0));
     vector<vector<int> > obecnaKolumna(n, vector<int>(k + 1, 0));
 
-    for (int j = min(grzyby[0][m - 1], k); j >= 0; j--) {
-        nastepnaKolumna[0][j] = 1;
+    if (grzyby[0][0]) {
+        obecnaKolumna[0][1]++;
+    } else {
+        obecnaKolumna[0][0]++;
     }
 
-    //DP na ostatnia kolumne
+    int ile = grzyby[0][0];
     for (int i = 1; i < n; i++) {
-        nastepnaKolumna[i] = nastepnaKolumna[i - 1];
-
-        int ile = grzyby[i][m - 1];
-
-        if (ile >= k || grzyby[i - 1][m - 1] == ile) {
-            continue;
+        if (!grzyby[i][0]) {
+            obecnaKolumna[i] = obecnaKolumna[i - 1];
+        } else {
+            ile = ile < k ? ile + 1 : k;
+            obecnaKolumna[i][ile]++;
         }
-        nastepnaKolumna[i][ile + 1] = 0;
     }
-    // for(int j=0; j<n; j++) {
-    //     //cout<<"nastepnaKolumna["<< j <<"] = ";
-    //     for(int i=0; i<=k; i++) {
-    //         cout<< nastepnaKolumna[j][i] <<" ";
-    //     }
-    //     cout<<endl;
-    // }
 
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < m; j++) {
-    //         if (i == n - 1)cout << grzyby[i][j] << " ";
-    //         else
-    //             cout << grzyby[i][j] - grzyby[i + 1][j]<< " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    // cout<< "Rozpoczynam przetwarzanie kolumn..." << n << m << k << g <<endl;
-    // fflush(stdout);
-
-    //Pokolei wyliczamy scierzki
-    for (int kolumna = m - 2; kolumna >= 0; kolumna--) {
-        for (int wyborPrawo = 0; wyborPrawo < n; wyborPrawo++) {
-            if (wyborPrawo > 0) {
-                int grzyby_nie_liczone = wyborPrawo < n - 1 ? grzyby[wyborPrawo + 1][kolumna] : 0;
-                if (grzyby[wyborPrawo - 1][kolumna] == grzyby_nie_liczone) {
-                    obecnaKolumna[wyborPrawo] = obecnaKolumna[wyborPrawo - 1];
-                    continue;
+    for (int kolumna = 1; kolumna < m; kolumna++) {
+        // Wylicz wartosci w nastepnej kolumnie po przejsciu bez posrednio z poprzedniej kolumny i zapisz wynik w obecnejKolumnie
+        for (int i = 0; i < n; i++) {
+            if (grzyby[i][kolumna]) {
+                for (int j = k - 1; j >= 1; j--) {
+                    nastepnaTemp[i][j] = obecnaKolumna[i][j - 1];
                 }
-            }
-
-            fill(obecnaKolumna[wyborPrawo].begin(), obecnaKolumna[wyborPrawo].end(), 0);
-
-            //dla kazdego kolejnego wyboru
-            for (int i = 0; i < n; i++) {
-                int nieliczoneGrzyby = max(wyborPrawo, i) < n - 1 ? grzyby[max(wyborPrawo, i) + 1][kolumna] : 0;
-                int nowe_zebrane_grzyby = grzyby[min(wyborPrawo, i)][kolumna] - nieliczoneGrzyby;
-
+                nastepnaTemp[i][k] = (obecnaKolumna[i][k - 1] + obecnaKolumna[i][k]) % MOD;
+                nastepnaTemp[i][0] = 0;
+            } else {
                 for (int j = 0; j <= k; j++) {
-                    int ktory = j - nowe_zebrane_grzyby < 0 ? 0 : j - nowe_zebrane_grzyby;
-                    obecnaKolumna[wyborPrawo][j] = (obecnaKolumna[wyborPrawo][j] + nastepnaKolumna[i][ktory]) % MOD;
+                    nastepnaTemp[i][j] = obecnaKolumna[i][j];
                 }
             }
         }
-        swap(obecnaKolumna, nastepnaKolumna);
+
+        nastepnaKolumna = nastepnaTemp;
+        obecnaKolumna = nastepnaKolumna;
+
+        for (int i = 1; i < n; i++) {
+            if (grzyby[i][kolumna] == false) {
+                for (int j = 0; j <= k; j++) {
+                    nastepnaTemp[i][j] = (nastepnaTemp[i][j] + nastepnaTemp[i - 1][j]) % MOD;
+                }
+            } else {
+                nastepnaTemp[i][0] = 0;
+                for (int j = 1; j <= k; j++) {
+                    nastepnaTemp[i][j] = (nastepnaTemp[i][j] + nastepnaTemp[i - 1][j - 1]) % MOD;
+                    if (j == k) {
+                        nastepnaTemp[i][j] = (nastepnaTemp[i][j] + nastepnaTemp[i - 1][j]) % MOD;
+                    }
+                }
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (grzyby[i][kolumna] == false) {
+                for (int j = 0; j <= k; j++) {
+                    nastepnaKolumna[i][j] = (nastepnaKolumna[i][j] + nastepnaKolumna[i + 1][j]) % MOD;
+                }
+            } else {
+                nastepnaKolumna[i][0] = 0;
+                for (int j = 1; j <= k; j++) {
+                    nastepnaKolumna[i][j] = (nastepnaKolumna[i][j] + nastepnaKolumna[i + 1][j - 1]) % MOD;
+                    if (j == k) {
+                        nastepnaKolumna[i][j] = (nastepnaKolumna[i][j] + nastepnaKolumna[i + 1][j]) % MOD;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                nastepnaKolumna[i][j] = (nastepnaKolumna[i][j] - obecnaKolumna[i][j] + MOD) % MOD;
+                nastepnaKolumna[i][j] = (nastepnaKolumna[i][j] + nastepnaTemp[i][j]) % MOD;
+            }
+        }
+        obecnaKolumna = nastepnaKolumna;
     }
 
-    int ILOSC_SCIERZEK = nastepnaKolumna[0][k];
-
-    cout << ILOSC_SCIERZEK << '\n';
+    cout << obecnaKolumna[n - 1][k] << '\n';
 
     return 0;
 }
