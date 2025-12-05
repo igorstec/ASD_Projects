d456669e9c8df3e047726562e20805e4e0c30358
igorstec <igor@tmsaudit.pl>
2025-12-05 15:43:46 +0100

    Fix maxval_ checking and implement compression for the tree capacity

--- Dzialki.cpp
+++ Dzialki.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <math.h>
 #include <vector>
 #include <algorithm>
 #include <tuple>
+#include <map>
 
 using namespace std;
 
@@ -11,14 +11,18 @@ class Tree {
     int idxval;
     vector<int> tree;
     int size;
+    map<int, int> compress_;
+    int maxval_;
 
 public:
-    Tree(int n) : idxval(2 * (1 << (31 - __builtin_clz(n)))), tree(2 * idxval, 0), size(2 * idxval) {
+    Tree(int n, map<int, int> compress, int maxval) : idxval(2 * (1 << (31 - __builtin_clz(n)))), tree(2 * idxval, 0), compress_(std::move(compress)), size(2 * idxval), maxval_(compress_[maxval]) {
     }
 
     void update(int idx, int val) {
-        if (idx < 0 || idx > size)
-            throw invalid_argument("Index out of bounds for update");
+        //udate tylko na realnych idx czyli
+        idx = compress_[idx];
+        if (idx < 0 || idx > idxval -1)
+            return;
         idx += idxval;
         tree[idx] += val;
         while (idx > 1) {
@@ -28,13 +32,13 @@ public:
     }
 
     int rangeQuery(int l, int r) {
-        if(l> idxval - 1)
+        l = compress_[l];
+        r = compress_[r];
+        if(l > maxval_)
             return 0;
-        if (r > idxval - 1)
-            r = idxval - 1;
+        if (r > maxval_)
+            r = maxval_;
 
-        if (l > r || l < 0 || r > size)
-            throw invalid_argument("Invalid range for query");
         l += idxval;
         r += idxval;
         int sum = 0;
@@ -55,52 +59,69 @@ public:
 };
 
 int main() {
-    int n, m;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, maxval = 0;
     cin >> n >> m;
-    vector<pair<int, int> > trees;
-    int maxy = 0;
+
+    vector<pair<int, int> > trees(n);
+    vector<int> all_y;
+
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        if (b > maxy)
-            maxy = b;
-        trees.push_back({a, b});
+        if(b>maxval)
+            maxval = b;
+        all_y.push_back(b);
+        trees[i] = {a, b};
     }
-    vector<int> results(m, 0);
+
     sort(trees.begin(), trees.end());
-    pair<int, tuple<int, int, int> > requests[2 * m];
-    Tree tree(maxy);
+
+    vector<tuple<int, int, int, int>> requests;
+
     for (int i = 0; i < 2 * m; i += 2) {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-        requests[i] = {a - 1, {c, d, i}};
-        requests[i + 1] = {b, {c, d, i + 1}};
+
+        requests.push_back( {a - 1, c, d, i});
+        requests.push_back({b, c, d, i + 1});
+        all_y.push_back(c);
+        all_y.push_back(d);
     }
-    sort(requests, requests + 2 * m);
-    //cout requests
-    // for(int i=0; i<2*m; i++){
-    //   cout<<requests[i].first<<" "<<get<0>(requests[i].second)<<" "<<get<1>(requests[i].second)<<"\n";
-    // }
+    sort(all_y.begin(), all_y.end());
+    all_y.erase(unique(all_y.begin(), all_y.end()), all_y.end());
+
+    sort(requests.begin(), requests.end());
+
+    map<int, int> compress;
+
+    for (int i = 0; i < all_y.size(); i++) {
+        compress[all_y[i]] = i;
+    }
+
+    Tree tree((int)all_y.size(), std::move(compress), maxval);
+
+    vector<int> results(m, 0);
     int tree_idx = 0;
     for (auto request: requests) {
 
-        while (tree_idx < n && trees[tree_idx].first <= request.first) {
+        while (tree_idx < n && trees[tree_idx].first <= get<0>(request)) {
             tree.update(trees[tree_idx].second, 1);
             tree_idx++;
         }
 
-        int l = get<0>(request.second);
-        int r = get<1>(request.second);
-        int idx = get<2>(request.second);
+        int l = get<1>(request);
+        int r = get<2>(request);
+        int idx = get<3>(request);
 
         if (idx % 2 == 0) {
-            // lewa granica
             results[idx / 2] -= tree.rangeQuery(l, r);
         } else {
             results[idx / 2] += tree.rangeQuery(l, r);
         }
     }
-
 
     for (int result: results) {
         cout << result << "\n";
