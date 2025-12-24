7b666fa373ac831e9bc58ed00f43aca3a9756372
igorstec <igor@tmsaudit.pl>
2025-12-24 15:38:22 +0100

    Fix Djiktra implementation and cover the memory losses

--- Motel.cpp
+++ Motel.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
-    int n, m, to_return = INT_MAX;
+    int n, m;
+    int to_return = INT_MAX;
     cin >> n >> m;
-    queue<int> verties_left;
+    priority_queue<pair<int,int>> verties_left; // cost to get to a vertix, vertix
     vector<pair<int, int> > distances(n, {INT_MAX, INT_MAX}); //cost to a from start, cost from a to end
-    verties_left.push(0);
-    vector<list<pair<int, int> > > graph(n);
-    vector<list<pair<int, int> > > inverted_graph(n);
-    vector<bool> visited(n, false);
+    verties_left.emplace(0, 0);
+    vector graph(n, vector<pair<int, int> >());
+    vector inverted_graph(n, vector<pair<int, int> >());
 
     for (int i = 0; i < m; i++) {
         int a, b, c;
@@ -26,40 +26,34 @@ int main() {
     }
     distances[0].first = 0;
     while (!verties_left.empty()) {
-        int current = verties_left.front();
+        int current = verties_left.top().second;
+        int current_cost = -verties_left.top().first;
         verties_left.pop();
-        visited[current] = true;
         for (auto [neighbour, cost]: graph[current]) {
-            int new_cost = distances[current].first + cost;
+            int new_cost = current_cost + cost;
             if (new_cost < distances[neighbour].first) {
                 distances[neighbour].first = new_cost;
+                verties_left.emplace(-distances[neighbour].first, neighbour);
             }
-            if (!visited[neighbour]) {
-                verties_left.push(neighbour);
-            }
+
         }
     }
-    fill(visited.begin(), visited.end(), false);
     distances[n - 1].second = 0;
-    verties_left.push(n - 1);
+    verties_left.emplace(0, n - 1);
     while (!verties_left.empty()) {
-        int current = verties_left.front();
+        int current = verties_left.top().second;
+        int current_cost = -verties_left.top().first;
         verties_left.pop();
-        visited[current] = true;
         for (auto [neighbour, cost]: inverted_graph[current]) {
-            int new_cost = distances[current].second + cost;
+            int new_cost = current_cost + cost;
             if (new_cost < distances[neighbour].second) {
                 distances[neighbour].second = new_cost;
-            }
-            if (!visited[neighbour]) {
-                verties_left.push(neighbour);
+                verties_left.emplace(-distances[neighbour].second, neighbour);
             }
         }
     }
     for (int i = 0; i < n; i++) {
-        if (distances[i].first != INT_MAX && distances[i].second != INT_MAX) {
-            to_return = min(to_return, max(distances[i].first, distances[i].second));
-        }
+        to_return = min(to_return, max(distances[i].first, distances[i].second));
     }
 
     cout << to_return << endl;
