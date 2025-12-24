7e2481ba4e770ea8c97e0368b3bfd62d91a68204
igorstec <igor@tmsaudit.pl>
2025-12-24 15:01:12 +0100

    Fix formatting

--- Motel.cpp
+++ Motel.cpp
@@ -14,8 +14,8 @@ int main() {
     queue<int> verties_left;
     vector<pair<int, int> > distances(n, {INT_MAX, INT_MAX}); //cost to a from start, cost from a to end
     verties_left.push(0);
-    vector<list<pair<int,int>> > graph(n);
-    vector<list<pair<int,int>> > inverted_graph(n);
+    vector<list<pair<int, int> > > graph(n);
+    vector<list<pair<int, int> > > inverted_graph(n);
     vector<bool> visited(n, false);
 
     for (int i = 0; i < m; i++) {
@@ -29,36 +29,36 @@ int main() {
         int current = verties_left.front();
         verties_left.pop();
         visited[current] = true;
-        for(auto [neighbour, cost] : graph[current]) {
+        for (auto [neighbour, cost]: graph[current]) {
             int new_cost = distances[current].first + cost;
-            if(new_cost < distances[neighbour].first) {
+            if (new_cost < distances[neighbour].first) {
                 distances[neighbour].first = new_cost;
             }
-            if(!visited[neighbour]) {
+            if (!visited[neighbour]) {
                 verties_left.push(neighbour);
             }
         }
     }
     fill(visited.begin(), visited.end(), false);
-    distances[n-1].second = 0;
-    verties_left.push(n-1);
+    distances[n - 1].second = 0;
+    verties_left.push(n - 1);
     while (!verties_left.empty()) {
         int current = verties_left.front();
         verties_left.pop();
         visited[current] = true;
-        for(auto [neighbour, cost] : inverted_graph[current]) {
+        for (auto [neighbour, cost]: inverted_graph[current]) {
             int new_cost = distances[current].second + cost;
-            if(new_cost < distances[neighbour].second) {
+            if (new_cost < distances[neighbour].second) {
                 distances[neighbour].second = new_cost;
             }
-            if(!visited[neighbour]) {
+            if (!visited[neighbour]) {
                 verties_left.push(neighbour);
             }
         }
     }
-    for(int i=0; i<n; i++) {
-        if(distances[i].first != INT_MAX && distances[i].second != INT_MAX) {
-            to_return = min(to_return, max(distances[i].first ,distances[i].second));
+    for (int i = 0; i < n; i++) {
+        if (distances[i].first != INT_MAX && distances[i].second != INT_MAX) {
+            to_return = min(to_return, max(distances[i].first, distances[i].second));
         }
     }
 
